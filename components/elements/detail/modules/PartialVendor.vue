a11874b697fa729f9c9b9891cf6c61b973fea775
vinhphamdev <vinhphamdev@gmail.com>
2021-02-10 10:08:18 +0700

    Update mapbox order-detail

--- components/elements/detail/modules/PartialVendor.vue
+++ components/elements/detail/modules/PartialVendor.vue
@@ -13,126 +13,35 @@ export default {
             default: () => {},
         },
     },
-    data() {
-        return {
-            map: null,
-            _from:null,
-            _to:null
-        }
-    },
     async created() {
         if (!this.product.vendor.address) return;
-
         mapboxgl.accessToken =
             'pk.eyJ1IjoicGhvbmduaGF0MTkiLCJhIjoiY2traWtzMXRrMjV4dzJvcGE5cHQ3MWJmaiJ9.ohDtLEc_AuCHfk1Ns3t8hA';
         let mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
-
-        const from = await mapboxClient.geocoding
+        mapboxClient.geocoding
             .forwardGeocode({
                 query: this.product.vendor.address,
                 autocomplete: false,
                 limit: 1,
-            }).send();
-
-            this._from = [-73.98598843989849, 40.75753761273267];
-
-         this._to = [-74.01306601045609, 40.712251474963686];
-
-        let feature = from.body.features[0];
-
-         this.map = new mapboxgl.Map({
+            })
+            .send()
+            .then(function (response) {
+                if (
+                    response &&
+                    response.body &&
+                    response.body.features &&
+                    response.body.features.length
+                ) {
+                    let feature = response.body.features[0];
+                    let map = new mapboxgl.Map({
                         container: 'map',
-                        style: 'mapbox://styles/mapbox/streets-v10',
-                        center: [-73.98598843989849, 40.75753761273267],
-                        zoom: 12,
+                        style: 'mapbox://styles/mapbox/streets-v11',
+                        center: feature.center,
+                        zoom: 15,
                     });
-
-                    this.map.getCanvasContainer();
-
-
-//   Add starting point to the map
-
-  this.map.on('load', () => {
-        this.getRoute();
-
-      this.map.addLayer({
-    id: 'point',
-    type: 'circle',
-    source: {
-      type: 'geojson',
-      data: {
-        type: 'FeatureCollection',
-        features: [{
-          type: 'Feature',
-          properties: {},
-          geometry: {
-            type: 'Point',
-            coordinates: this._from
-          }
-        }
-        ]
-      }
-    },
-    paint: {
-      'circle-radius': 10,
-      'circle-color': '#3887be'
-    }
-  });
-  })
-    },
-
-    methods: {
-
-    async getRoute() {
-        const params = {
-            start1: this._from[0],
-            start2: this._from[1],
-            end1: this._to[0],
-            end2: this._to[1]
-        }
-
-        const response = await this.$store.dispatch('auth/getMap', params);
-
-    var route = response.routes[0].geometry.coordinates;
-    var geojson = {
-      type: 'Feature',
-      properties: {},
-      geometry: {
-        type: 'LineString',
-        coordinates: route
-      }
-    };
-    // if the route already exists on the map, reset it using setData
-    if (this.map.getSource('route')) {
-      this.map.getSource('route').setData(geojson);
-    } else { // otherwise, make a new request
-    console.log('elseeeeee');
-      this.map.addLayer({
-        id: 'route',
-        type: 'line',
-        source: {
-          type: 'geojson',
-          data: {
-            type: 'Feature',
-            properties: {},
-            geometry: {
-              type: 'LineString',
-              coordinates: geojson
-            }
-          }
-        },
-        layout: {
-          'line-join': 'round',
-          'line-cap': 'round'
-        },
-        paint: {
-          'line-color': '#3887be',
-          'line-width': 5,
-          'line-opacity': 0.75
-        }
-      });
-    }
-}
+                    new mapboxgl.Marker().setLngLat(feature.center).addTo(map);
+                }
+            });
     },
 };
 </script>
--- pages/order/_id.vue
+++ pages/order/_id.vue
@@ -96,6 +96,8 @@
                                     </div>
                                 </div>
                             </div>
+
+                            <div id="map"></div>
                         </div>
                     </div>
                 </div>
@@ -136,12 +138,71 @@ export default {
             orderNumber: '',
             status: '',
             createdAt: '',
+
+            map: null,
+            _from: null,
+            _to: null,
         };
+    },
+
+    methods: {
+        async getRoute() {
+            const params = {
+                start1: this._from[0],
+                start2: this._from[1],
+                end1: this._to[0],
+                end2: this._to[1],
+            };
+
+            const response = await this.$store.dispatch('auth/getMap', params);
+
+            var route = response.routes[0].geometry.coordinates;
+            var geojson = {
+                type: 'Feature',
+                properties: {},
+                geometry: {
+                    type: 'LineString',
+                    coordinates: route,
+                },
+            };
+            // if the route already exists on the map, reset it using setData
+            if (this.map.getSource('route')) {
+                this.map.getSource('route').setData(geojson);
+            } else {
+                // otherwise, make a new request
+                this.map.addLayer({
+                    id: 'route',
+                    type: 'line',
+                    source: {
+                        type: 'geojson',
+                        data: {
+                            type: 'Feature',
+                            properties: {},
+                            geometry: {
+                                type: 'LineString',
+                                coordinates: route,
+                            },
+                        },
+                    },
+                    layout: {
+                        'line-join': 'round',
+                        'line-cap': 'round',
+                    },
+                    paint: {
+                        'line-color': '#3887be',
+                        'line-width': 5,
+                        'line-opacity': 0.75,
+                    },
+                });
+            }
+        },
     },
 
     async created() {
         const id = this.$route.params.id;
         await this.$store.dispatch('shop/getOrderDetail', id);
+
+        console.log(this.orderDetail);
 
         this.tableData = this.orderDetail.order_items;
         this.total = this.orderDetail.invoice_amount;
@@ -151,6 +212,72 @@ export default {
         this.orderNumber = this.orderDetail.order_number;
         this.status = this.orderDetail.order_status;
         this.createdAt = this.orderDetail.createdAt;
+
+        mapboxgl.accessToken =
+            'pk.eyJ1IjoicGhvbmduaGF0MTkiLCJhIjoiY2traWtzMXRrMjV4dzJvcGE5cHQ3MWJmaiJ9.ohDtLEc_AuCHfk1Ns3t8hA';
+        let mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
+
+        const from = await mapboxClient.geocoding
+            .forwardGeocode({
+                query: this.orderDetail.vendor.address,
+                autocomplete: false,
+                limit: 1,
+            })
+            .send();
+
+        this._from = from.body.features[0].center;
+
+        console.log(this._from);
+
+        const to = await mapboxClient.geocoding
+            .forwardGeocode({
+                query: this.orderDetail.shipping_address,
+                autocomplete: false,
+                limit: 1,
+            })
+            .send();
+
+        this._to = to.body.features[0].center;
+
+        this.map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v10',
+            center: this._from,
+            zoom: 12,
+        });
+
+        this.map.getCanvasContainer();
+
+        //   Add starting point to the map
+
+        this.map.on('load', () => {
+            this.getRoute();
+
+            this.map.addLayer({
+                id: 'point',
+                type: 'circle',
+                source: {
+                    type: 'geojson',
+                    data: {
+                        type: 'FeatureCollection',
+                        features: [
+                            {
+                                type: 'Feature',
+                                properties: {},
+                                geometry: {
+                                    type: 'Point',
+                                    coordinates: this._from,
+                                },
+                            },
+                        ],
+                    },
+                },
+                paint: {
+                    'circle-radius': 10,
+                    'circle-color': '#3887be',
+                },
+            });
+        });
     },
 };
 </script>
@@ -169,5 +296,11 @@ export default {
 
 p {
     margin-bottom: 0;
+}
+
+#map {
+    width: 100%;
+    height: 400px;
+    margin:20px 0px
 }
 </style>
