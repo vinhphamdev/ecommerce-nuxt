bf8856046ae0ce1d7989f46fdfc839f4f4e65a2d
devzom <dev.zomerfeld@gmail.com>
2021-02-17 20:30:10 +0100

    fix:Wrong blog array name to get newest posts

--- store/blog.js
+++ store/blog.js
@@ -6,7 +6,7 @@ export const state = () => ({
 
 export const getters = {
     getAllPosts: state => state.blog,
-    getNewestPost: state => state.blog.slice(0, 5)
+    getNewestPost: state => state.blog.posts.slice(0, 5)
 };
 
 export const mutations = {};
