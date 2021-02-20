d320fc32f7dd0d9e52e90103744e313741e47189
devzom <dev.zomerfeld@gmail.com>
2021-02-20 23:30:00 +0100

    add:Dynamic RatingComponent

--- components/elements/Rating.vue
+++ components/elements/Rating.vue
@@ -1,16 +1,29 @@
 <template lang="html">
     <span class="ps-rating">
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star-o"></i>
+        <i v-for='(checkedStar, index) in rating' :key='index' class="fa fa-star"></i>
+        <i v-for='(uncheckedStar, index) in starsUnchecked' :key='index' class="fa fa-star-o"></i>
     </span>
 </template>
 
 <script>
 export default {
-    name: 'Rating'
+    name: 'Rating',
+	props: {
+		rating: {
+			type: Number,
+			default: 0
+		}
+	},
+	data() {
+		return {
+			maxRating: 5
+		}
+	},
+	computed: {
+		starsUnchecked() {
+			return this.maxRating - this.rating
+		}
+	}
 };
 </script>
 
