67d51214a0db8709c65734334134c2e049d0d86c
devzom <dev.zomerfeld@gmail.com>
2021-04-14 21:56:23 +0200

    fix:error with babel/preset-env

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,26 +6,26 @@ export default {
             { charset: 'utf-8' },
             {
                 name: 'viewport',
-                content: 'width=device-width, initial-scale=1'
+                content: 'width=device-width, initial-scale=1',
             },
             {
                 hid: 'description',
                 name: 'description',
-                content: 'Martfury - Multipurpose Marketplace Vuejs Ecommerce Template'
-            }
+                content: 'Martfury - Multipurpose Marketplace Vuejs Ecommerce Template',
+            },
         ],
         link: [
             {
                 rel: 'stylesheet',
-                href: 'https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700&amp;amp;subset=latin-ext'
+                href: 'https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700&amp;amp;subset=latin-ext',
             },
             {
                 rel: 'stylesheet',
-                href: 'https://api.mapbox.com/mapbox-gl-js/v2.0.0/mapbox-gl.css'
-            }
+                href: 'https://api.mapbox.com/mapbox-gl-js/v2.0.0/mapbox-gl.css',
+            },
         ],
 
-        script: [{ src: 'https://js.stripe.com/v3' }]
+        script: [{ src: 'https://js.stripe.com/v3' }],
     },
 
     css: [
@@ -33,7 +33,7 @@ export default {
         '~/static/fonts/Linearicons/Font/demo-files/demo.css',
         '~/static/fonts/font-awesome/css/font-awesome.css',
         '~/static/css/bootstrap.min.css',
-        '~/assets/scss/style.scss'
+        '~/assets/scss/style.scss',
     ],
 
     plugins: [
@@ -42,13 +42,23 @@ export default {
         { src: '~/plugins/vue-notification.js', ssr: false },
         { src: '~/plugins/axios.js', ssr: false },
         { src: '~/plugins/lazyLoad.js', ssr: false },
-        { src: '~/plugins/localStorage.js', ssr: false }
+        { src: '~/plugins/localStorage.js', ssr: false },
     ],
 
     buildModules: ['@nuxtjs/vuetify', '@nuxtjs/style-resources', 'cookie-universal-nuxt'],
+    /*
+     ** Build configuration
+     */
+    build: {
+        babel: {
+            presets(env, [preset, options]) {
+                return [['@babel/preset-env', options]]
+            },
+        },
+    },
 
     styleResources: {
-        scss: './assets/scss/env.scss'
+        scss: './assets/scss/env.scss',
     },
 
     modules: ['@nuxtjs/axios', 'nuxt-i18n'],
@@ -56,20 +66,20 @@ export default {
     i18n: {
         locales: [
             { code: 'en', file: 'en.json' },
-            { code: 'fr', file: 'fr.json' }
+            { code: 'fr', file: 'fr.json' },
         ],
         lazy: true,
         defaultLocale: 'en',
-        langDir: 'lang/locales/'
+        langDir: 'lang/locales/',
     },
 
     router: {
         linkActiveClass: '',
-        linkExactActiveClass: 'active'
+        linkExactActiveClass: 'active',
     },
 
     server: {
         port: 4002,
-        host: 'localhost'
-    }
-};
+        host: 'localhost',
+    },
+}
